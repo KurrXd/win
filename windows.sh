829a321c6b53afca0c5571009583b5e8fed4ef1e
KurrXd <91126186+KurrXd@users.noreply.github.com>
2023-01-02 14:47:40 +0700

    Update windows.sh

--- windows.sh
+++ windows.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
-echo "╭━━━╮╱╱╱╱╱╱╱╱╱╱╭╮╭╮╭╮╱╱╱╱╱╭╮╱╱╱╱╱╱╱╱╱╱╱╭━━━┳━━━╮"
-echo "┃╭━╮┃╱╱╱╱╱╱╱╱╱╱┃┃┃┃┃┃╱╱╱╱╱┃┃╱╱╱╱╱╱╱╱╱╱╱┃╭━╮┃╭━╮┃"
-echo "┃╰━╯┣╮╱╭┳━━┳━╮╱┃┃┃┃┃┣┳━╮╭━╯┣━━┳╮╭╮╭┳━━╮┃┃╱┃┃╰━━╮"
-echo "┃╭╮╭┫┃╱┃┃╭╮┃╭╮╮┃╰╯╰╯┣┫╭╮┫╭╮┃╭╮┃╰╯╰╯┃━━┫┃┃╱┃┣━━╮┃"
-echo "┃┃┃╰┫╰━╯┃╭╮┃┃┃┃╰╮╭╮╭┫┃┃┃┃╰╯┃╰╯┣╮╭╮╭╋━━┃┃╰━╯┃╰━╯┃"
-echo "╰╯╰━┻━╮╭┻╯╰┻╯╰╯╱╰╯╰╯╰┻╯╰┻━━┻━━╯╰╯╰╯╰━━╯╰━━━┻━━━╯"
-echo "╱╱╱╱╭━╯┃"
-echo "╱╱╱╱╰━━╯"
-echo "SCRIPT AUTO INSTALL WINDOWS by Ryan"
+echo ".  .         \  /  ."
+echo "|_/ . .._.._. ><  _|"
+echo "|  \(_|[  [  /  \(_]:
+echo
+echo "GITHUB : KurrXd"
 echo
 echo "Pilih OS yang ingin anda install"
 echo "[1] Windows 2019(Default)"
 echo "[2] Windows 2016"
 echo "[3] Windows 2012"
 echo "[4) Windows 10"
-echo "[5] Chat Ryan Untuk Add OS lain"
+echo "[5] Chat KurrXd Untuk Add OS lain"
 
 read -p "Pilih [1]: " PILIH OS
 
@@ -58,7 +54,7 @@ EOF
 
 cat >/tmp/dpart.bat<<EOF
 @ECHO OFF
-echo Ryan Store
+echo KurrXd
 echo JENDELA INI JANGAN DITUTUP
 echo SCRIPT INI AKAN MERUBAH PORT RDP MENJADI 5000, UNTUK MENYAMBUNG KE RDP GUNAKAN ALAMAT $IP4:5000
 echo KETIK YES LALU ENTER!
@@ -113,5 +109,5 @@ cp -f /tmp/dpart.bat dpart.bat
 ██║░░██║██║░░██║██║╚████║██╔══╝░░
 ██████╔╝╚█████╔╝██║░╚███║███████╗
 ╚═════╝░░╚════╝░╚═╝░░╚══╝╚══════╝
-echo "reboot Rdp dulu mazzeh baru bisa pake"
+echo "reboot dulu mazzeh baru bisa pake"
 
