bd6f1975b09dbb6e3af2432a501f76869e2e2709
KurrXd <91126186+KurrXd@users.noreply.github.com>
2023-01-02 14:59:19 +0700

    Update windows.sh

--- windows.sh
+++ windows.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo "GITHUB : KurrXd"
+echo "SCRIPT AUTO INSTALL WINDOWS by KurrXd"
 echo
 echo "Pilih OS yang ingin anda install"
 echo "[1] Windows 2019(Default)"
 echo "[2] Windows 2016"
 echo "[3] Windows 2012"
-echo "[4] Windows 10"
+echo "[4) Windows 10"
 echo "[5] Chat KurrXd Untuk Add OS lain"
 
 read -p "Pilih [1]: " PILIH OS
@@ -50,7 +50,7 @@ EOF
 
 cat >/tmp/dpart.bat<<EOF
 @ECHO OFF
-echo KurrXd
+echo KurrXd Store
 echo JENDELA INI JANGAN DITUTUP
 echo SCRIPT INI AKAN MERUBAH PORT RDP MENJADI 5000, UNTUK MENYAMBUNG KE RDP GUNAKAN ALAMAT $IP4:5000
 echo KETIK YES LALU ENTER!
@@ -105,5 +105,4 @@ cp -f /tmp/dpart.bat dpart.bat
 ██║░░██║██║░░██║██║╚████║██╔══╝░░
 ██████╔╝╚█████╔╝██║░╚███║███████╗
 ╚═════╝░░╚════╝░╚═╝░░╚══╝╚══════╝
-echo "reboot dulu mazzeh baru bisa pake"
-
+echo "reboot Rdp dulu mazzeh baru bisa pake"
